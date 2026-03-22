47f1577c466c49c6d05d1e81a1ac2869ddfa6162
Mihai <mikisapunaru@gmail.com>
2026-03-22 12:47:12 +0200

    New urls in setting for frontendl

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -165,3 +165,8 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://merry-maamoul-abfa02.netlify.app"
+    "http://localhost:3000",                   # Pentru teste locale
+    "http://127.0.0.1:3000",
+]
